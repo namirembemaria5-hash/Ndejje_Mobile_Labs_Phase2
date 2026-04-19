c881b6af33474cc133e93310eaa79359d17fdbb7
Namirembe Margaret <namirembemaria5@gmail.com>
2026-04-19 16:40:25 +0300

    added shape.kt file

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -24,16 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.ndejje.momocalc.R.string.fee_label
-import com.ndejje.momocalc.ui.theme.MoMoCalculatorAppTheme
 import com.ndejje.momocalc.ui.theme.MoMoTopBar
-import com.ndejje.momocalc.ui.theme.MoMoTypography
+import android.content.res.Configuration
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -152,7 +149,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 
 
 
